e638cba14b3044cb6ac55f4a910e343c9634dfb5
iamaditya <adity1s2h@gmail.com>
2020-12-18 20:07:50 +0530

    pattern added

---
+++ pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    printf("Printing pattern for %d size \n", n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
